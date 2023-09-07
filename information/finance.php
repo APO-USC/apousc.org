4b6e56101ba0b1b09cb7bd2720d38c2e49edfbdf
Owen Hsieh <ohsieh@usc.edu>
2023-09-07 15:07:39 -0700

    Update finance.php Fall2023

--- information/finance.php
+++ information/finance.php
@@ -29,47 +29,48 @@ include_once("include/topnav.php");
 ?>
 <br />
 			<h2>Finance</h2>
-			<p>If you don't see what you're looking for, just contact Mia Ward or Justin Yang!</p>
+			<p>If you don't see what you're looking for, just contact Elodie Zhen and Leon Yoshida!p>
 
                 <h3>Spring 2023 Information</h3>
                 <ul>
-                    	<li><a href='https://docs.google.com/spreadsheets/d/1u3NZUGQtLHa8qYS0PNRcGdzf8e7MN0guaO4pRiLJIh4/edit#gid=1852648581' target = "_blank">Budget</a></li>
+                    	<li><a href='https://docs.google.com/spreadsheets/d/1sQPCbvDb7I1TUUPZouZOJDQXC8QR1lqcqm3KYOZe04w/edit#gid=1852648581' target = "_blank">Budget</a></li>
                     	<li>Venmo: @apoakusc</li>
                     	<li>Email: <a href="mailto:finance.apousc@gmail.com">finance.apousc@gmail.com</a></li>
-			<li><a href='https://docs.google.com/forms/d/e/1FAIpQLSd_b4XEwhT7dcoJNLOIugXLLhTbx4DxtfZOomC2yYXRU-KG7w/viewform' target = "_blank">New Members: Financial Aid Application</a></li>
-			<li><a href='https://docs.google.com/spreadsheets/d/1qCJAXGA4SQR949Pjb46hK6SJYdtXqzFA8UmVkKBN90U/edit#gid=0' target = "_blank">Dues Breakdown</a></li>
-			<li><a href='https://docs.google.com/forms/d/e/1FAIpQLSdFeGECmGHAN1zH573kYCmJSMcGS96kcvJxOM003j2FHuu6Bg/viewform' target = "_blank">Payment Plan Adjustment Request</a></li>
-			<li><a href='https://docs.google.com/spreadsheets/d/1PXuN3l-g1iufoe9mkM1dk4oTXMJSzb-WA9KnPFyFqRw/edit#gid=0' target = "_blank">Extra Fundraising Point Oppprtunities - Spring 2023</a></li>
-<!-- 			<li><a href='https://docs.google.com/spreadsheets/d/1x9XIh9mjRXdK5HI4_aqES2yVP0TQ-4vDHp-YxXfPsJo/edit?usp=sharing' target = "_blank">Semester Calendar</a></li> -->
+			<li><a href='https://docs.google.com/forms/d/e/1FAIpQLScfZJ_-fkXlfMOEyBHXxZmMeDUi6mde1QfDwD9dyMQRzXxVvA/viewform' target = "_blank">New Members: Financial Aid Application</a></li>
+			<li><a href='https://docs.google.com/spreadsheets/d/14caTP6ABRb9NMGnrHJyy0u_Ub2aZz0ChzbkRq0uvyP0/edit#gid=0' target = "_blank">Dues Breakdown</a></li>
+			<li><a href='https://forms.gle/sTVAQgdq4QZmtM3r9' target = "_blank">Payment Plan Adjustment Request</a></li>
+<!-- 			<li><a href='https://docs.google.com/spreadsheets/d/1PXuN3l-g1iufoe9mkM1dk4oTXMJSzb-WA9KnPFyFqRw/edit#gid=0' target = "_blank">Extra Fundraising Point Oppprtunities - Spring 2023</a></li>
+<!-- 			<li><a href='https://docs.google.com/spreadsheets/d/1x9XIh9mjRXdK5HI4_aqES2yVP0TQ-4vDHp-YxXfPsJo/edit?usp=sharing' target = "_blank">Semester Calendar</a></li> --> -->
                 </ul>
                 
                 <h3>Dues</h3>
-                <p>Please contact Mia Ward or Justin Yang if you need additional financial accommodations for our payment plans.</p>
+                <p>Please contact Elodie Zhen or Leon Yoshida if you need additional financial accommodations for our payment plans.</p>
 		<p>Payments due @ 11:59 PM of each deadline or your account will be frozen :(</p>
-		<h4><b>Actives: $135 early bird, $140 regular</b></h4>
+		<h4><b>Actives: $141 early bird, $146 regular</b></h4>
                 <h4><b>Associates: $55 regular</b></h4>
-		<h4><b>New Members: $275 regular</b></h4>
+		<h4><b>New Members: $270 regular</b></h4>
 		<p><b>Actives Payment Plan</b></p>	
                 <ul>
-			<li>Pay full February 6: $135</li>
-                    	<li>February 6: $50</li>
-                    	<li>February 20: $45</li>
-                    	<li>March 6: $45</li>
+			<li>Pay full September 18: $146</li>
+                    	<li>September 18: $49</li>
+                    	<li>October 2: $49</li>
+                    	<li>October 16: $48</li>
             	</ul>
 
              	<p><b>Associates Payment Plan</b></p>	
                 <ul>
-			<li>Pay full February 6: $55</li>
-                    	<li>February 6: $20</li>
-                	<li>February 20: $20</li>
-                    	<li>March 6: $15</li>
+			<li>Pay full September 18: $55</li>
+                    	<li>September 18: $20</li>
+                	<li>October 2: $20</li>
+                    	<li>October 16: $15</li>
             	</ul>
 
 		<p><b>New Members Payment Plan</b></p>	
                 <ul>
-                    	<li>February 27: $95</li>
-                	<li>March 13: $90</li>
-                    	<li>March 27: $90</li>
+                    	<li>Pay in full October 9: $270</li>
+                	<li>October 9: $90</li>
+                    	<li>October 23: $90</li>
+			<li>November 6: $90</li>
             	</ul>
 				<p><b>*If you do not pay your dues and do not communicate with the VPs, we will have to freeze your account until the matter is resolved.</b></p>
 				
@@ -77,9 +78,9 @@ include_once("include/topnav.php");
 				
 				<p>In order to get reimbursed, please send your receipts to Mia Ward or Justin Yang at finance.apousc@gmail.com.</p>
 		<ul>
-                    <li><a href='https://forms.gle/njp842MmXoSDrUXm8' target = "_blank">Driver Reimbursement Car Information Form</a></li>
-		    <li><a href='https://forms.gle/fDck9aCVq8nETHsm9' target = "_blank">Driver Reimbursement Request</a></li>
-		    <li><a href='https://docs.google.com/forms/d/e/1FAIpQLSfF-O_SJuQ4HWXIEfsT8CzNuR2u6YDEMQBUYQWv09nxcsH3Aw/viewform' target = "_blank">General APO Reimbursement Form</a></li>
+                    <li><a href='https://forms.gle/LsasfhVn2RPSQraD8' target = "_blank">Driver Reimbursement Car Information Form</a></li>
+		    <li><a href='https://forms.gle/TrXuVXKV2uYKusQd7' target = "_blank">Driver Reimbursement Request</a></li>
+		    <li><a href='https://forms.gle/1DPYTvujFbHPYfpv5' target = "_blank">General APO Reimbursement Form</a></li>
                 </ul>
 				
 <br />
